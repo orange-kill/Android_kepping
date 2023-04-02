23becc994990d096c8ccdf3cd0fe4335ba15686c
huwenrun <1577955547@qq.com>
2023-04-02 14:31:21 +0800

    hwr(FIRST）

---
+++ app/src/main/java/cn/itcast/keeping/ui/things/ThingsFragment.java
@@ -0,0 +1,39 @@
+package cn.itcast.keeping.ui.things;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import cn.itcast.keeping.databinding.FragmentThingsBinding;
+
+public class ThingsFragment extends Fragment {
+    private FragmentThingsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ThingsViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(ThingsViewModel.class);
+
+
+
+        binding = FragmentThingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textThings;//在fragment_things.xml中的ID
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
