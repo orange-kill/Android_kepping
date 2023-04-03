d157683cf2649c7945ee3ebee649b376964cd697
xiaowangbaby <2377292966@qq.com>
2023-04-03 23:09:39 +0800

    登录页面基本交互

---
+++ app/src/main/java/cn/itcast/keeping/login/ChoseCircleActivity.java
@@ -0,0 +1,32 @@
+package cn.itcast.keeping.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import cn.itcast.keeping.R;
+
+public class ChoseCircleActivity extends AppCompatActivity {
+    private ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose_circle);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        String number = intent.getStringExtra("phoneNumber");
+        String name = intent.getStringExtra("userName");
+        String sex = intent.getStringExtra("sex");
+    }
+}
---
+++ app/src/main/java/cn/itcast/keeping/login/ChoseSexActivity.java
@@ -0,0 +1,57 @@
+package cn.itcast.keeping.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import cn.itcast.keeping.R;
+
+public class ChoseSexActivity extends AppCompatActivity {
+    private ImageView back;
+    private ImageView male;
+    private ImageView female;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose_sex);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        String number = intent.getStringExtra("phoneNumber");
+        String name = intent.getStringExtra("userName");
+
+        male = findViewById(R.id.male);
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("sex","male");
+                intent.setClass(ChoseSexActivity.this,ChoseCircleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        female = findViewById(R.id.female);
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("sex","female");
+                intent.putExtra("phoneNumber",number);
+                intent.putExtra("uerName",name);
+                intent.setClass(ChoseSexActivity.this,ChoseCircleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
